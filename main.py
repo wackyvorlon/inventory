29380a91deed2861a9668f0a0d92c8f7f6463efb
Paul Anderson <ander1x@uwindsor.ca>
2013-05-06 20:36:30 -0400

    Not behaving right with class setup.

--- main.py
+++ main.py
@@ -2,16 +2,28 @@
 
 from Tkinter import *
 
-root = Tk()
-
-w = Label(root, text="Hello, world!")
-w.pack()
-
-root.mainloop()
 
 class App(object):
-	"""docstring for App"""
+	"""A simple inventory app."""
 	def __init__(self, arg):
 		super(App, self).__init__()
 		self.arg = arg
+
+		frame = Frame(master)
+        frame.pack()
+
+        self.button = Button(frame, text="QUIT", fg="red", command=frame.quit)
+        self.button.pack(side=LEFT)
+
+        self.hi_there = Button(frame, text="Hello", command=self.say_hi)
+        self.hi_there.pack(side=LEFT)
+
+        def say_hi(self):
+        	print "Hello, world!"
+
 		
+root = Tk()
+
+app = App(root)
+
+root.mainloop()
